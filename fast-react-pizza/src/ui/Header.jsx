410af82ad38c98dbe3e7349e989f0ab31b6270a5
Ribtas007 <ribtas007@outlook.com>
2024-02-12 18:11:03 +0530

    completed adding tailwind

--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -1,13 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
 
 const Header = () => {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header
+      className=" border-b-300 font-pizza flex items-center 
+    justify-around border border-stone-500 bg-yellow-500 px-4 py-3 uppercase sm:px-6"
+    >
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
-      <p>Jonas</p>
+      <Username />
     </header>
   );
 };
